4e0ab0226f3fe6f5a4297f0d00ee2388702a2624
Francesco Tabaro <francesco.tabaro@gmail.com>
2020-03-23 16:18:33 +0200

    reformat

--- scripts/make_config.py
+++ scripts/make_config.py
@@ -41,123 +41,123 @@ def make_parser():
 
     ## Mandatory options
     parser.add_argument("--config-path",
-        required=True,
-        help="Desired path for the config file")
+                        required=True,
+                        help="Desired path for the config file")
 
     parser.add_argument("--wd",
-        required=True,
-        help="Working directory")
+                        required=True,
+                        help="Working directory")
 
     parser.add_argument("--genome-path",
-        required=True,
-        help="Path to a fasta file with genome sequence")
+                        required=True,
+                        help="Path to a fasta file with genome sequence")
 
     parser.add_argument("--bismark-index-path",
-        required=True,
-        help="Path to Bismark index files (only index name required: /path/to/index/folder/hg38*)")
+                        required=True,
+                        help="Path to Bismark index files (only index name required: /path/to/index/folder/hg38*)")
 
     parser.add_argument("--sample-sheet",
-        required=True,
-        help="Path to csv file holding samples information")
+                        required=True,
+                        help="Path to csv file holding samples information")
 
     parser.add_argument("--annotation-file",
-        required=True,
-        help="Path to a gzipped GTF file.")
+                        required=True,
+                        help="Path to a gzipped GTF file.")
 
     ## Numeric parameters for DMR detection
     parser.add_argument("--dmr-window-size",
-        default=500,
-        help="Window size for tiled differential methylation analysis")
+                        default=500,
+                        help="Window size for tiled differential methylation analysis")
 
     parser.add_argument("--dmr-step-size",
-        default=500,
-        help="Step size for tiled differential methylation analysis")
+                        default=500,
+                        help="Step size for tiled differential methylation analysis")
 
     parser.add_argument("--dmr-difference",
-        default=25,
-        help="Difference in reads coverage threshold for differential methylation analysis")
+                        default=25,
+                        help="Difference in reads coverage threshold for differential methylation analysis")
 
     parser.add_argument("--dmr-qvalue",
-        default=0.01,
-        help="Q-value threshold for differential methylation analysis")
+                        default=0.01,
+                        help="Q-value threshold for differential methylation analysis")
 
     parser.add_argument("--min-per-group",
-        default=1,
-        help="An integer denoting minimum number of samples per replicate needed to cover a region/base")
+                        default=1,
+                        help="An integer denoting minimum number of samples per replicate needed to cover a region/base")
 
     ## Patterns and string parameters
     parser.add_argument("--mate1-pattern",
-        default="_1",
-        help="Pattern to identify mate 1 in paired sequencing files")
+                        default="_1",
+                        help="Pattern to identify mate 1 in paired sequencing files")
 
     parser.add_argument("--mate2-pattern",
-        default="_2",
-        help="Pattern to identify mate 2 in paired sequencing files")
+                        default="_2",
+                        help="Pattern to identify mate 2 in paired sequencing files")
 
     parser.add_argument("--fastq-extension",
-        default=".fq.gz",
-        help="File extension of reads fastq files")
+                        default=".fq.gz",
+                        help="File extension of reads fastq files")
 
     parser.add_argument("--genome-version",
-        default="hg38",
-        help="Label for genome version used in the analysis")
+                        default="hg38",
+                        help="Label for genome version used in the analysis")
 
     ## Optional paths
     parser.add_argument("--singularity-container",
-        default="/home/ft413468/containers/methylkit.sif",
-        help="Path to a singularity container with all necessary tools installed.")
+                        default="/home/ft413468/containers/methylkit.sif",
+                        help="Path to a singularity container with all necessary tools installed.")
 
     parser.add_argument("--tmp-folder",
-        default="tmp",
-        help="Path to a temporary folder (possibly fast storage)")
+                        default="tmp",
+                        help="Path to a temporary folder (possibly fast storage)")
 
     parser.add_argument("--log-folder",
-        default="~/var/log",
-        help="Path to a log folder")
+                        default="~/var/log",
+                        help="Path to a log folder")
 
     parser.add_argument("--reads-folder",
-        default="reads",
-        help="Path to a folder with reads to be processed")
+                        default="reads",
+                        help="Path to a folder with reads to be processed")
 
     parser.add_argument("--trimmed-folder",
-        default="trimmed",
-        help="Path to folder to write trimmed reads in")
+                        default="trimmed",
+                        help="Path to folder to write trimmed reads in")
 
     parser.add_argument("--alignments-folder",
-        default="alignments",
-        help="Path to a folder to write alignments in")
+                        default="alignments",
+                        help="Path to a folder to write alignments in")
 
     parser.add_argument("--reports-folder",
-        default="reports",
-        help="Path to a folder to write Bismark reports in")
+                        default="reports",
+                        help="Path to a folder to write Bismark reports in")
 
     parser.add_argument("--nucleotide-stats-folder",
-        default="nucleotide_stats",
-        help="Path to write Bismark nucleotide report files in")
+                        default="nucleotide_stats",
+                        help="Path to write Bismark nucleotide report files in")
 
     parser.add_argument("--methylkitdb-folder",
-        default="NULL",
-        help="Path to write methylKit tabix files in")
+                        default="NULL",
+                        help="Path to write methylKit tabix files in")
 
     parser.add_argument("--rdata-folder",
-        default="RData",
-        help="Path to write RDS objects used in methylKit analysis")
+                        default="RData",
+                        help="Path to write RDS objects used in methylKit analysis")
 
     parser.add_argument("--bed-folder",
-        default="bed",
-        help="Path to write BED files for DMR/DMC coordinates to")
+                        default="bed",
+                        help="Path to write BED files for DMR/DMC coordinates to")
 
     parser.add_argument("--pictures-folder",
-        default="pictures",
-        help="Path to a folder to write plots in")
+                        default="pictures",
+                        help="Path to a folder to write plots in")
 
     parser.add_argument("--tables-folder",
-        default="tables",
-        help="Path to a folder to write tables in")
+                        default="tables",
+                        help="Path to a folder to write tables in")
 
     parser.add_argument("--fastqc-folder",
-        default="fastqc",
-        help="FastQC results folder")
+                        default="fastqc",
+                        help="FastQC results folder")
 
     parser.add_argument("--low-coverage-count",
                         default=10,
@@ -176,7 +176,6 @@ def test_relative_path(p):
 
 
 def make_env(args):
-
     for k in ["config_path", "wd", "genome_path", "bismark_index_path", "sample_sheet"]:
         if test_relative_path(args[k]) and args[k] != "-":
             args[k] = os.path.abspath(args[k])
@@ -190,7 +189,7 @@ def make_env(args):
 
         if test_relative_path(args[k]):
             new = os.path.join(args["wd"], args[k])
-            print("Setting {}: {} -> {}".format( k, args[k], new))
+            print("Setting {}: {} -> {}".format(k, args[k], new))
             args[k] = new
 
         if not os.path.exists(args[k]):
@@ -227,7 +226,7 @@ def write_config(args):
 
 
 if __name__ == "__main__":
-        args = make_parser()
-        args = make_env(args)
-        args["samples"] = parse_samples(args["sample_sheet"])
-        write_config(args)
+    args = make_parser()
+    args = make_env(args)
+    args["samples"] = parse_samples(args["sample_sheet"])
+    write_config(args)
